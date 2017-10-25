33a6eb7c6c438371ae35d2ef3aed4beabe1ef7da
manubhagarg <manubha9@gmail.com>
2017-10-25 23:38:17 +0530

    profile dummy view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root to: "home#index"
+  root to: "home#index"
+
+  get 'profile/index' 
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
